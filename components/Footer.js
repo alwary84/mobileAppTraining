65ee976911d333f80b5c9f50b76d7254700a8883
alwary84 <alwary84@gmail.com>
2021-07-13 15:29:31 +0800

    refactor

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Footer = () => {
+    return(
+    <View style={{flex:1, flexDirection:"row"}}>
+       <View style={{flex:1, backgroundColor: "yellow"}}></View>
+       <View style={{flex:1, backgroundColor: "white"}}></View>
+     </View>
+    )
+}
+
+export default Footer;
---
+++ homeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function HomeScreen() {
+  return (
+  <View style={{flex:1, }}>
+
+     <Header />
+
+    <View style={{flex:5, backgroundColor:"red"}}></View>
+
+    <Footer />
+
+  </View>
+  );
+}
+
+
