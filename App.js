df0965f0fb85686a33aecfa4bf1ae674f3246f3d
alwary84 <alwary84@gmail.com>
2021-07-13 12:25:43 +0800

    using flex to create layout

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <View style={{flex:1, }}>
+
+     <View style={{flex:1, flexDirection:"row"}}>
+       <View style={{flex:1, backgroundColor: "yellow"}}></View>
+       <View style={{flex:1, backgroundColor: "purple"}}></View>
+       <View style={{flex:1, backgroundColor: "blue"}}></View>
+     </View>
+
+    <View style={{flex:5, backgroundColor:"red"}}>
     </View>
+
+    <View style={{flex:1, flexDirection:"row"}}>
+       <View style={{flex:1, backgroundColor: "yellow"}}></View>
+       <View style={{flex:1, backgroundColor: "purple"}}></View>
+     </View>
+
+  </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
