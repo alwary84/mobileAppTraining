de7601e8a9fa8ae246dda5f9cac72227d4c3dc05
alwary84 <alwary84@gmail.com>
2021-07-13 15:31:10 +0800

    refactor1

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
+import Homescreen from "./screens/Homescreen"
 
 export default function App() {
-  return (
-  <View style={{flex:1, }}>
-
-     <View style={{flex:1, flexDirection:"row"}}>
-       <View style={{flex:1, backgroundColor: "yellow"}}></View>
-       <View style={{flex:1, backgroundColor: "purple"}}></View>
-       <View style={{flex:1, backgroundColor: "blue"}}></View>
-     </View>
-
-    <View style={{flex:5, backgroundColor:"red"}}>
-    </View>
-
-    <View style={{flex:1, flexDirection:"row"}}>
-       <View style={{flex:1, backgroundColor: "yellow"}}></View>
-       <View style={{flex:1, backgroundColor: "purple"}}></View>
-     </View>
-
-  </View>
-  );
+  return <Homescreen/>;
 }
 
 
